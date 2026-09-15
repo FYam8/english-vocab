ccf1893ef18e2722b4476437dbde58a02946deec
FYam8 <gentle.green.shade@gmail.com>
2026-09-16 04:28:54 +0900

    refactor: introduce explicit Waseda persistence adapter config

--- src/waseda-bootstrap/15-waseda-persistence.js
+++ src/waseda-bootstrap/15-waseda-persistence.js
@@ -1,2 +1,3 @@
-const SCHEMA_VERSION=7;
-const STORAGE_KEY="waseshibu_vocab_state";
+const WASEDA_APP_CONFIG=Object.freeze({schemaVersion:7,storageKey:"waseshibu_vocab_state"});
+const SCHEMA_VERSION=WASEDA_APP_CONFIG.schemaVersion;
+const STORAGE_KEY=WASEDA_APP_CONFIG.storageKey;
