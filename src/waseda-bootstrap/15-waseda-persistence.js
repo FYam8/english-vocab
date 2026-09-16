1a25b6f4220a70d40fc80d6df607773a06e23354
FYam8 <gentle.green.shade@gmail.com>
2026-09-16 14:54:53 +0900

    refactor: add active-session persistence config to Waseda adapter

--- src/waseda-bootstrap/15-waseda-persistence.js
+++ src/waseda-bootstrap/15-waseda-persistence.js
@@ -1,3 +1,8 @@
-const WASEDA_APP_CONFIG=Object.freeze({schemaVersion:7,storageKey:"waseshibu_vocab_state"});
+const WASEDA_APP_CONFIG=Object.freeze({
+  schemaVersion:7,
+  storageKey:"waseshibu_vocab_state",
+  activeSessionKey:"waseshibu_vocab_active_session_v1",
+  activeSessionFormatVersion:1
+});
 const SCHEMA_VERSION=WASEDA_APP_CONFIG.schemaVersion;
 const STORAGE_KEY=WASEDA_APP_CONFIG.storageKey;
